4fc82420fdf01f50ce16edec26ec6edf2f15454d
brucejh99 <bjhong@yahoo.com>
2018-11-12 03:25:39 -0500

    Added field validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Form.css';
 
 export default class Form extends Component {
@@ -7,41 +8,67 @@ export default class Form extends Component {
     this.state = {
         nameValue: '',
         emailValue: '',
-        messageValue: ''
+        messageValue: '',
+        validName: true,
+        validEmail: true,
+        validMessage: true
     }
-    this.handleChange = this.handleChange.bind(this);
+    this.handleName = this.handleName.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit (e) {
-      console.log('Form value: ' + this.state.inputValue);
-      // prevent default only in case of if invalid form
+    // regex pattern to validate email
+    const isValidEmail = (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(this.state.emailValue));
+    const isValidName = (this.state.nameValue !== '');
+    const isValidMessage = (this.state.messageValue !== '');
+    if(isValidEmail && isValidName && isValidMessage) {
+      axios.get('https://www.mocky.io/v2/5bd89817310000c63a474f5b')
+        .then(response => console.log(response));
+    } else {
       e.preventDefault();
+      if(!isValidName) this.setState({ validName: false });
+      if(!isValidEmail) this.setState({ validEmail: false });
+      if(!isValidMessage) this.setState({ validMessage: false });
+    }
   }
   
-  handleChange (e) {
+  handleName (e) {
       this.setState({
-          inputValue: e.target.value,
-          emailValue: e.target.value,
-          messageValue: e.target.value
+          nameValue: e.target.value,
       })
   }
 
+  handleEmail (e) {
+    this.setState({
+        emailValue: e.target.value,
+    })
+  }
+
+  handleMessage (e) {
+    this.setState({
+        messageValue: e.target.value,
+    })
+  }
+
+  // make buttons red if validX is false
   render() {
     return (
-      <form className="formContainer" onSubmit={this.handleSubmit.bind(this)}>
+      <form className="formContainer" onSubmit={this.handleSubmit}>
         <h2 className="formTitle">Contact Us</h2>
-        <div className="smallSectionContainer">
-          <label className="smallSectionText">Name</label>
-          <input className="smallSectionInput" type="text" value={this.state.inputValue} onChange={this.handleChange.bind(this)}/>
+        <div className="sectionContainer-small sectionContainer">
+          <label className="sectionText-small sectionText">Name</label>
+          <input className="sectionInput-small sectionInput" type="text" value={this.state.nameValue} onChange={this.handleName}/>
         </div>
-        <div className="smallSectionContainer">
-          <label className="smallSectionText">Email</label>
-          <input className="smallSectionInput" type="text" value={this.state.emailValue} onChange={this.handleChange.bind(this)}/>
+        <div className="sectionContainer-small sectionContainer">
+          <label className="sectionText-small sectionText">Email</label>
+          <input className="sectionInput-small sectionInput" type="text" value={this.state.emailValue} onChange={this.handleEmail}/>
         </div>
-        <div className="largeSectionContainer">
-          <label className="largeSectionText">Message</label>
-          <input className="largeSectionInput" type="text" value={this.state.messageValue} onChange={this.handleChange.bind(this)}/>
+        <div className="sectionContainer-large sectionContainer">
+          <label className="sectionText-large sectionText">Message</label>
+          <textarea className="sectionInput-large sectionInput" type="text" value={this.state.messageValue} onChange={this.handleMessage}/>
         </div>
         <input className="submitButton" type="submit" value="Submit"/>
       </form>
