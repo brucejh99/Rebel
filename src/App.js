d86538fc5f2b6358599b416af1dd639facd43d38
brucejh99 <bjhong@yahoo.com>
2018-11-12 12:32:20 -0500

    Made API calls customizable and red invalid fields

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Form from './components/Form';
 import './App.css';
 
 class App extends Component {
-  /* other content above and below form */
+  /* Other content above and below form. Insert the form anywhere on the page. Form was designed to always accept the same information with customizable API urls */
   render() {
     return (
       <div className="shift">
-        <Form />
+        <Form successUrl='https://www.mocky.io/v2/5bd89817310000c63a474f5b' failUrl='https://www.mocky.io/v2/5bd897fc310000c73a474f59' />
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -13,6 +13,8 @@ export default class Form extends Component {
         validEmail: true,
         validMessage: true
     }
+    this.successUrl = props.successUrl;
+    this.failUrl = props.failUrl;
     this.handleName = this.handleName.bind(this);
     this.handleEmail = this.handleEmail.bind(this);
     this.handleMessage = this.handleMessage.bind(this);
@@ -25,13 +27,19 @@ export default class Form extends Component {
     const isValidName = (this.state.nameValue !== '');
     const isValidMessage = (this.state.messageValue !== '');
     if(isValidEmail && isValidName && isValidMessage) {
-      axios.get('https://www.mocky.io/v2/5bd89817310000c63a474f5b')
+      axios.get(this.successUrl)
         .then(response => console.log(response));
+        // handle whatever should come after a successful submission here
     } else {
       e.preventDefault();
       if(!isValidName) this.setState({ validName: false });
+      else this.setState({ validName: true });
       if(!isValidEmail) this.setState({ validEmail: false });
+      else this.setState({ validEmail: true });
       if(!isValidMessage) this.setState({ validMessage: false });
+      else this.setState({ validMessage: true });
+      axios.get(this.failUrl)
+        .then(response => console.log(response));
     }
   }
   
@@ -53,22 +61,27 @@ export default class Form extends Component {
     })
   }
 
-  // make buttons red if validX is false
   render() {
     return (
       <form className="formContainer" onSubmit={this.handleSubmit}>
         <h2 className="formTitle">Contact Us</h2>
         <div className="sectionContainer-small sectionContainer">
           <label className="sectionText-small sectionText">Name</label>
-          <input className="sectionInput-small sectionInput" type="text" value={this.state.nameValue} onChange={this.handleName}/>
+          { this.state.validName ? 
+          <input className="sectionInput-small sectionInput" type="text" value={this.state.nameValue} onChange={this.handleName}/> :
+          <input className="sectionInput-small sectionInputFailed" type="text" value={this.state.nameValue} onChange={this.handleName}/> }
         </div>
         <div className="sectionContainer-small sectionContainer">
           <label className="sectionText-small sectionText">Email</label>
-          <input className="sectionInput-small sectionInput" type="text" value={this.state.emailValue} onChange={this.handleEmail}/>
+          { this.state.validEmail ? 
+          <input className="sectionInput-small sectionInput" type="text" value={this.state.emailValue} onChange={this.handleEmail}/> :
+          <input className="sectionInput-small sectionInputFailed" type="text" value={this.state.emailValue} onChange={this.handleEmail}/> }
         </div>
         <div className="sectionContainer-large sectionContainer">
           <label className="sectionText-large sectionText">Message</label>
-          <textarea className="sectionInput-large sectionInput" type="text" value={this.state.messageValue} onChange={this.handleMessage}/>
+          { this.state.validMessage ? 
+          <textarea className="sectionInput-large sectionInput" type="text" value={this.state.messageValue} onChange={this.handleMessage}/> :
+          <textarea className="sectionInput-large sectionInputFailed" type="text" value={this.state.messageValue} onChange={this.handleMessage}/> }
         </div>
         <input className="submitButton" type="submit" value="Submit"/>
       </form>
